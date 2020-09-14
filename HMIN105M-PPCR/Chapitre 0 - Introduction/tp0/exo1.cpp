392405a3ddcaf1e748bccaad13248810cdc560a0
Alex <canton.alex@live.fr>
2020-09-14 18:59:39 +0200

    rename folders

---
+++ HMIN105M-PPCR/Chapitre 0 - Introduction/tp0/exo1.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std; 
+
+int main() {
+  int a = 10;
+  int b = 25;
+  int* p = &b;
+  int* pp = &a;
+
+  // 1. 25 valeur de b
+  //cout << *(&(*(*(&p))));
+
+  // 2. -858993460 adresse de a 
+  //cout << *(p - 1);
+
+  // 3. -858993460 adresse de a
+  //cout << *(*(&p) - 1);
+
+  // 4. -858993460 adresse de a
+  //cout << *(*(&pp) + 1);
+
+  // 5. -858993460 adresse de a
+  //cout << *(&(*(*(&p))) - 1);
+
+  // 6. -858993460 adresse de a
+  cout << *(&(*(*(&p))) - 1);
+}
