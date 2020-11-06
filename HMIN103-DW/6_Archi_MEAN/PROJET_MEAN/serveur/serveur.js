8513f86e4d87e2677a547bbca681d364b6bae24d
Alex <canton.alex@live.fr>
2020-11-06 13:04:58 +0100

    début tp MEAN

--- HMIN103-DW/6_Archi_MEAN/PROJET_MEAN/serveur/serveur.js
+++ HMIN103-DW/6_Archi_MEAN/PROJET_MEAN/serveur/serveur.js
@@ -1,5 +1,6 @@
 const express = require('express');
-const app     = express();
+const app = express();
+
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(function (req, res, next) {
