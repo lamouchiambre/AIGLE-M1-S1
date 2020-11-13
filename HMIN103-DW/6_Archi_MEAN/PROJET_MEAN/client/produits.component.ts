4be09b7f0dea38d08c0febca87cda2ae5c39101c
Alex <canton.alex@live.fr>
2020-11-13 19:32:28 +0100

    tp map reduce

---
+++ HMIN103-DW/6_Archi_MEAN/PROJET_MEAN/client/produits.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+import { ProduitsService } from '../produits.service';
+import { Observable } from 'rxjs';
+
+@Component({
+  selector: 'app-produits',
+  templateUrl: './produits.component.html',
+  styleUrls: ['./produits.component.css']
+})
+export class ProduitsComponent implements OnInit {
+    private produits: Object[];
+    
+    constructor(private produitsService: ProduitsService) {
+       console.log("Dans le constructeur du composant produits");
+    }
+    
+    ngOnInit() {
+       console.log("Dans ngOnInit() du composant produits");    
+       this.produitsService.getProduits().subscribe(produits => {
+            this.produits = produits;
+       });
+    }
+}
