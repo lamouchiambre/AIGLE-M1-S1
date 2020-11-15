68cceb634de41009b6758b742760029dcb915295
Alexandre Canton Condes <canton.alex@live.fr>
2020-11-15 12:51:49 +0100

    update

--- HMIN105M-PPCR/Chap2-IPC/1-parking/ipc.c
+++ HMIN105M-PPCR/Chap2-IPC/1-parking/ipc.c
@@ -2,39 +2,43 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <sys/ipc.h>
-#include <sys/msg.h>
+// #include <sys/msg.h>
 #include <sys/shm.h>
 #include <sys/types.h>
-
-// typedef struct parking {
-//   int places;
-// } parking;
+#include <sys/stat.h>
+#include <fcntl.h>
 
 int main(int argc, char* argv[]) {
 
-  char* file = "parking";
   int places = atoi(argv[1]);
-  
-  key_t key = ftok("key.txt", 'z');
+
+  printf("Création du fichier partagé\n");
+  int fd = open("parking_shared.txt", O_RDWR | O_CREAT, 0666);
+  close(fd);
+
+  printf("Création de la clé d'accès\n");
+  key_t key = ftok("parking_shared.txt", 1);
   if (key == -1) {
     perror("ftok");
     exit(1);
   } 
-  printf("ftok ok\n");
 
-  int shm_id = shmget(key, sizeof(int), IPC_CREAT | 0666);
+  printf("Création du segment mémoire partagé\n");
+  // deux elements : un pour le nb de place actuel et un pour le max
+  int shm_id = shmget(key, sizeof(int) * 2, IPC_CREAT | 0666);
+  printf("Le segment est %i\n", shm_id);
   if (shm_id == -1) {
     perror("shmget");
     exit(1);
   } 
-  printf("shmget ok\n");
 
+  printf("Initialisation de la variable partagée\n");
   int* nb_places = (int*)shmat(shm_id, NULL, 0);
+  printf("L'addresse du nombre de places est %i\n", *nb_places);
   if ((*nb_places) == -1) {
     perror("shmat");
     exit(1);
   } 
-  printf("shmat ok\n");
 
   if (shmdt((void*)nb_places) == -1) {
     perror("shmdt");
