4fa3f16997155c3f6ed30efa31b2740247e6be84
Alex <canton.alex@live.fr>
2020-11-17 10:59:49 +0100

    modif

---
+++ HMIN102-IL/TP9-1/src/il_tp9_state/CalculetteUnknownOperator.java
@@ -0,0 +1,30 @@
+package il_tp9_state;
+
+public class CalculetteUnknownOperator extends Exception {
+
+	public CalculetteUnknownOperator() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public CalculetteUnknownOperator(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	public CalculetteUnknownOperator(Throwable cause) {
+		super(cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public CalculetteUnknownOperator(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public CalculetteUnknownOperator(String message, Throwable cause, boolean enableSuppression,
+			boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+		// TODO Auto-generated constructor stub
+	}
+
+}
